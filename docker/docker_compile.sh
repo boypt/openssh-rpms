46090eddbef7b22b66b571f0d6fd37cba17bed84
boypt <1033514+boypt@users.noreply.github.com>
2026-04-09 04:11:52 +0000

    fix: docker compile script dir

--- docker/docker_compile.sh
+++ docker/docker_compile.sh
@@ -2,5 +2,6 @@
 DOCKER_BUILD_DIR=/BUILD
 mkdir -p $DOCKER_BUILD_DIR
 ELDIR=$(./compile.sh GETEL)
+[[ $ELDIR == el8 ]] && ELDIR=el7
 cp -r $ELDIR $DOCKER_BUILD_DIR
 ./compile.sh $DOCKER_BUILD_DIR/$ELDIR
