53207f21b524f8c3f479296abd1cb22940419abb
boypt <1033514+boypt@users.noreply.github.com>
2026-04-10 13:16:03 +0800

    docker: use system openssl if versiono 3

--- docker/docker_compile.sh
+++ docker/docker_compile.sh
@@ -2,6 +2,14 @@
 DOCKER_BUILD_DIR=/BUILD
 mkdir -p $DOCKER_BUILD_DIR
 ELDIR=$(./compile.sh GETEL)
+OPENSSLVER=$(rpm -q openssl --qf "%{VERSION}" | cut -d. -f1)
+
+if [[ $OPENSSLVER -ge 3 ]]; then
+  export WITH_OPENSSL=1
+else
+  export WITH_OPENSSL=2
+fi
+
 [[ $ELDIR == el8 ]] && ELDIR=el7
 cp -r $ELDIR $DOCKER_BUILD_DIR
 ./compile.sh $DOCKER_BUILD_DIR/$ELDIR
