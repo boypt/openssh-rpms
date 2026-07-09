018d9130abf3c7e969aa8b0b12afb11a82f8edc5
boypt <1033514+boypt@users.noreply.github.com>
2026-07-09 13:48:41 +0800

    fix: krb5 in el5 hack

--- compile.sh
+++ compile.sh
@@ -122,6 +122,10 @@ BUILD_RPM() {
         SOURCES+=($PERLSRC)
         RPMBUILDOPTS+=('--define' "perlver ${PERLVER}" '--define' 'dist .el5')
         export CC=gcc44
+
+	# Hack: old krb5 fail the test with openssl3.x, but works with openssh
+	export ac_cv_func_krb5_free_error_message=yes
+	export ac_cv_func_krb5_get_error_message=yes
     fi
 
     # add dist variable if not defined
