4bb5d616cfdd71ee46bc32661401428abd4f7aa1
Dely <dph5199278@163.com>
2026-08-05 19:49:34 +0800

    refactor: change el5-el8 yum mirror

--- docker/modify_yum_source.sh
+++ docker/modify_yum_source.sh
@@ -8,95 +8,45 @@
 RELEASE_VER=$(rpm --eval '%{?dist}')
 [ -z "$RELEASE_VER" ] && RELEASE_VER=".el5"
 
-# Cent OS 5 is NOT support modern SSL protocol, so use plain HTTP protocol.
-if [ "$RELEASE_VER" != ".el5" ]; then
+function modify_el() {
+  if [ "$MIRROR" != "0" ]; then
+    # Using 163 mirror, which is much useful for Chinese users.
+    MIRROR_URL="mirrors.163.com/centos-vault";
+  else
+    # Default mirror, the nsc mirror link.
+    MIRROR_URL="mirror.nsc.liu.se/centos-store";
+  fi
+
+  sed -e "s|mirror.nsc.liu.se/centos-store|${MIRROR_URL}|g" \
+      /etc/yum.repos.d/CentOS-*.repo && \
+  rm -rf /var/cache/yum/ && \
+  yum makecache
+}
+
+function modify_amzn() {
   if [ "$MIRROR" != "0" ]; then
     # Using USTC mirror, which is much useful for Chinese users.
-    MIRROR_URL="https://mirrors.ustc.edu.cn/centos-vault";
     AWS_DOMAIN="amazonaws.com.cn"
     AWS_REGION="cn-northwest-1"
   else
     # Default mirror, the official mirror link.
-    MIRROR_URL="https://vault.centos.org/";
     AWS_DOMAIN="amazonaws.com"
     AWS_REGION="us-east-2"
   fi
-  else
-    MIRROR_URL="http://mirrors.huaweicloud.com"
-fi
 
-# For ARM platform, the mirror url needs a suffix `altarch`
-if [ "$(uname -m)" = "aarch64" ]; then
-  OS_KEY="altarch"
-  if [ "$RELEASE_VER" != ".el8" ]; then
-    MIRROR_URL="$MIRROR_URL/$OS_KEY";
-  fi
-else
-  OS_KEY="centos"
-fi
-
-function modify_el8() {
-  sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-      -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=${MIRROR_URL}/8.5.2111|g" \
-      -i.bak /etc/yum.repos.d/CentOS-*.repo && \
-  rm -rf /var/cache/yum/ && \
-  yum makecache timer
-}
-
-function modify_el7() {
-  sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-  -e "s@^#baseurl=http://mirror.centos.org/$OS_KEY/\$releasever@baseurl=${MIRROR_URL}/7.9.2009@g" \
-  -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=${MIRROR_URL}/7.9.2009|g" \
-      -i.bak /etc/yum.repos.d/CentOS-*.repo && \
-  rm -rf /var/cache/yum/ && \
-  yum makecache fast
-}
-
-function modify_el6() {
-  sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-      -e "s|^#baseurl=http://mirror.centos.org/$OS_KEY/\$releasever|baseurl=${MIRROR_URL}/6.10|g" \
-      -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=${MIRROR_URL}/6.10|g" \
-      -i.bak /etc/yum.repos.d/CentOS-*.repo && \
-  rm -rf /var/cache/yum/ && \
-  yum makecache fast
-}
-
-function modify_el5() {
-  sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-      -e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=${MIRROR_URL}/centos-vault/5.11|g" \
-      -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=${MIRROR_URL}/centos-vault/5.11|g" \
-      -i.bak /etc/yum.repos.d/*.repo && \
-  rm -rf /var/cache/yum/ && \
-  yum makecache fast
+  [ -d /etc/yum/vars ] && echo "$AWS_DOMAIN" > /etc/yum/vars/awsdomain && echo "$AWS_REGION" > /etc/yum/vars/awsregion
+  [ -d /etc/dnf/vars ] && echo "$AWS_DOMAIN" > /etc/dnf/vars/awsdomain && echo "$AWS_REGION" > /etc/dnf/vars/awsregion
 }
 
 case $RELEASE_VER in
-  .el8)
-    modify_el8
+  .el*)
+    modify_el
     ;;
-  .el7)
-    modify_el7
-    ;;
-  .el6)
-    modify_el6
-    ;;
-  .el5)
-    modify_el5
-    ;;
-  .amzn1)
-    echo "$AWS_DOMAIN" > /etc/yum/vars/awsdomain
-    echo "$AWS_REGION" > /etc/yum/vars/awsregion
-    ;;
-  .amzn2)
-    echo "$AWS_DOMAIN" > /etc/yum/vars/awsdomain
-    echo "$AWS_REGION" > /etc/yum/vars/awsregion
-    ;;
-  .amzn2023)
-    echo "$AWS_DOMAIN" > /etc/dnf/vars/awsdomain
-    echo "$AWS_REGION" > /etc/dnf/vars/awsregion
+  .amzn*)
+    modify_amzn
     ;;
   *)
-    echo "rpm dist undefined, please specify: el5 el6 el7"
+    echo "rpm dist undefined, please specify: el5 el6 el7 el8"
     exit 1
     ;;
 esac
